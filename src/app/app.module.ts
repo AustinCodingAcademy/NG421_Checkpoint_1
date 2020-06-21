4e1906b4902872ea0aa3699dac42bc9bd0b4d661
AmyTroutman <amyrtroutman@gmail.com>
2020-06-21 10:09:45 -0500

    Filtering works. Input boxes match.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,12 @@ import { FormsModule } from '@angular/forms';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { ConfirmationModalComponent } from './confirmation-modal/confirmation-modal.component';
 import {RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TodoTableComponent } from './todo-table/todo-table.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatSortModule } from '@angular/material/sort';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 
 import { AppComponent } from './app.component';
@@ -12,11 +18,7 @@ import { TodoListComponent } from './todo-list/todo-list.component';
 import { CreateTodoComponent } from './create-todo/create-todo.component';
 import { BoardComponent } from './board/board.component';
 import { TodoEditComponent } from './todo-edit/todo-edit.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { TodoTableComponent } from './todo-table/todo-table.component';
-import { MatTableModule } from '@angular/material/table';
-import { MatSortModule } from '@angular/material/sort';
-import { MatFormFieldModule } from '@angular/material/form-field';
+
 
 const routes = [];
 const appRoutes = RouterModule.forRoot(routes);
@@ -40,7 +42,8 @@ const appRoutes = RouterModule.forRoot(routes);
     BrowserAnimationsModule,
     MatTableModule,
     MatSortModule,
-    MatFormFieldModule
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent],
